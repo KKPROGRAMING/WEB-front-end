eec9276a6bd135477437d9b35eb2f3f4efbd894a
KKPROGRAMING <2844359985@qq.com>
2022-09-08 11:09:26 +0800

    rename and new files

---
+++ javascript/referenceType/Array/fill().js
@@ -0,0 +1,58 @@
+/***
+ *      使用fill()方法可以向一个已有的数组中插入全部或部分相同的值。
+ * 
+ *      参数：
+ *          （1）第一个参数永远是【要填充进去的值】。
+ *              注意不要和表示位置的索引混淆。
+ *          （2）第二个参数可选，表示填充处的开始索引。
+ *              如果不提供开始索引，默认从索引为0处开始填充。
+ *          （3）第三个参数可选，表示填充处的结束索引。
+ *              如果不提供结束索引，则一直填充到数组末尾。
+ * 
+ *      另，负值索引从数组末尾开始计算。
+ *      也可以将负索引想象成数组长度加上它得到的一个正索引。
+ */
+
+const zeros = [0,0,0,0,0,0];
+
+//整个填充
+zeros.fill(7);
+console.log(zeros); //[ 7, 7, 7, 7, 7, 7 ]
+zeros.fill(0); 
+
+//索引大于等于3
+zeros.fill(7,3);
+console.log(zeros); //[ 0, 0, 0, 7, 7, 7 ]
+zeros.fill(0); 
+
+//索引大于等于1,小于4
+zeros.fill(7,1,4);
+console.log(zeros); //[ 0, 7, 7, 7, 0, 0 ]
+zeros.fill(0); 
+
+//负值索引
+// (-6 + zeros.length = 0)
+// (-3 + zeros.length = 3)
+zeros.fill(7,-6,-3);
+console.log(zeros); //[ 7, 7, 7, 0, 0, 0 ]
+zeros.fill(0); 
+
+//索引过低，忽略
+zeros.fill(7,-9,-7);
+console.log(zeros); //[ 0, 0, 0, 0, 0, 0 ]
+zeros.fill(0); 
+
+//索引过高，忽略
+zeros.fill(7,9,12);
+console.log(zeros); //[ 0, 0, 0, 0, 0, 0 ]
+zeros.fill(0); 
+
+//索引反向，忽略
+zeros.fill(7,4,2);
+console.log(zeros); //[ 0, 0, 0, 0, 0, 0 ]
+zeros.fill(0); 
+
+//索引部分可用，填充可用部分
+zeros.fill(7,3,10);
+console.log(zeros); //[ 0, 0, 0, 7, 7, 7 ]
+zeros.fill(0); 
--- javascript/referenceType/Array/iterator.js
+++ javascript/referenceType/Array/iterator.js
@@ -1,4 +1,50 @@
 /***
  * 在ES6中，Array的原型上暴露了3个用于【检索数组内容】的方法：
  *      keys() / values() / entries()
+ * 
+ *      1.keys()
+ *          返回数组【索引】的迭代器。
+ *      2.values()
+ *          返回数组【元素】的迭代器。
+ *      3.entries()
+ *          返回索引/值【对】的迭代器。
+ * 
+ *      由于这些方法返回的都是迭代器，
+ *      所以可以将它们的内容通过Array.from()直接转换为数组实例。
  */
+
+const arr = ["black","white","red","purple","blue","green"];
+
+const arr_keys = arr.keys(); //Object [Array Iterator] {}
+const arr_values = arr.values(); //Object [Array Iterator] {}
+const arr_entries = arr.entries(); //Object [Array Iterator] {}
+
+console.log(Array.from(arr_keys)); //[ 0, 1, 2, 3, 4, 5 ]
+console.log(Array.from(arr_values)); //[ 'black', 'white', 'red', 'purple', 'blue', 'green' ]
+console.log(Array.from(arr_entries));
+// [
+//     [ 0, 'black' ],
+//     [ 1, 'white' ],
+//     [ 2, 'red' ],
+//     [ 3, 'purple' ],
+//     [ 4, 'blue' ],
+//     [ 5, 'green' ]
+//   ]
+
+//使用ES6的解构可以容易地在循环中拆分键值对
+for (const [index,element] of arr.entries()) {
+    console.log(index);
+    console.log(element);
+}
+// 0
+// black
+// 1
+// white
+// 2
+// red
+// 3
+// purple
+// 4
+// blue
+// 5
+// green
